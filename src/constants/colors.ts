151d3ac9e801a27c8aece5bf11c88a97028d55f7
hnifalii <ankso30561@gmail.com>
2026-01-12 04:45:53 +0700

    chore: add constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,44 @@
+export const Colors = {
+  // Primary colors
+  primary: '#728C69',
+  // primaryDark: '#0051D5',
+  // primaryLight: '#5AC8FA',
+  
+  // Secondary colors
+  secondary: '#BCD5CF',
+  // secondaryDark: '#3634A3',
+  // secondaryLight: '#AF52DE',
+
+  // Accent colors
+  accent1: '#FBA359',
+  
+  // Neutral colors
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#8E8E93',
+  grayLight: '#C7C7CC',
+  grayDark: '#48484A',
+  
+  // Background colors
+  background: '#F2F2F7',
+  // backgroundDark: '#1C1C1E',
+  surface: '#FFFFFF',
+  // surfaceDark: '#2C2C2E',
+  
+  // Status colors
+  success: '#34C759',
+  warning: '#FF9500',
+  error: '#FF3B30',
+  info: '#5AC8FA',
+  
+  // Text colors
+  text: '#000000',
+  textSecondary: '#8E8E93',
+  textDisabled: '#C7C7CC',
+  
+  // Border colors
+  border: '#E5E5EA',
+  // borderDark: '#38383A',
+};
+
+export type ColorKey = keyof typeof Colors;
